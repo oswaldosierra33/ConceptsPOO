7c18330321c303e3bb14ca0ac989413d6736281a
Oswaldo Sierra <oswaldo.sierra@swat-ingenieria.com>
2026-01-16 17:23:31 -0500

    Class 1, Data Class

---
+++ ConceptsPOO/ConceptsPOO/DayException.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ConceptsPOO
+{
+    [Serializable]
+    internal class DayException : Exception
+    {
+        public DayException()
+        {
+        }
+
+        public DayException(string? message) : base(message)
+        {
+        }
+
+        public DayException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceptsPOO;
+
+Console.WriteLine("POO Concepts!");
+Console.WriteLine("==============");
+
+
+//Date date1 = new Date(2026, 01, 16);
+try
+{
+	Console.WriteLine(new Date(2024, 2, 29));
+	Console.WriteLine(new Date(1974, 9, 3));
+	Console.WriteLine(new Date(1985, 11, 10));
+	Console.WriteLine(new Date(1960, 12, 31));
+}
+catch (Exception error)
+{
+		Console.WriteLine(error.Message);
+}
